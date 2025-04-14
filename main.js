e486fcb5292ae80b2a9a9f61b1fccf04293c9136
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-04-14 19:14:59 +0330

    fixed bug

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const input = document.getElementById("input");
 input.addEventListener("keyup", function (event) {
-  if (event.value.length < 8) {
-    console.log(event.value);
+  if (event.target.length < 8) {
+    console.log(event.target);
 
     console.log(
       "Min 8 characters with at insert one capital  letter , a number and a special character"
     );
-  } else if (isNaN(event.value.length === 8)) {
+  } else if (isNaN(event.value === 8)) {
     console.log("password is good");
   }
 });

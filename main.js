4588e4603a2d3c9376471ef0a2a6f059092eb889
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-04-15 10:32:26 +0330

    check for 8 char and capital letter

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const input = document.getElementById("input");
-input.addEventListener("keyup", function (event) {
-  if (event.target.length < 8) {
-    console.log(event.target);
 
-    console.log(
-      "Min 8 characters with at insert one capital  letter , a number and a special character"
-    );
-  } else if (isNaN(event.value === 8)) {
-    console.log("password is good");
+input.addEventListener("change", function (event) {
+  const valueInputLenght = event.target.value.length;
+  const valueInput = event.target.value;
+  console.log(typeof event.target.value);
+
+  if (valueInputLenght < 8) {
+    const pTage = document.createElement("p");
+    pTage.textContent =
+      "Min 8 characters with at insert one capital letter , a number and special character";
+    document.body.appendChild(pTage);
+    //     pTage.appendChild(para);
+  } else if (valueInput) {
+    // valueInput.split("");
+    let text = valueInput;
+    let hasCapital = false;
+
+    text.split("").forEach((char) => {
+      if (char === char.toUpperCase() && char !== char.toLowerCase()) {
+        hasCapital = true;
+      }
+    });
+    if (!hasCapital) {
+      const pTag = document.createElement("p");
+      pTag.textContent = "Insert one capital letter";
+      document.body.appendChild(pTag);
+    }
   }
 });

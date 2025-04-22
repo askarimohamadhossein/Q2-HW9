cd53dc035c2bdc2d54e9ef7d7aa292ea461a5ffb
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-04-22 16:00:15 +0330

    Defining variables related to the password form

---
+++ main.js
@@ -0,0 +1,3 @@
+const passwordInput = document.getElementById("password");
+const toggleButton = document.getElementById("togglePassword");
+const errorMessage = document.getElementById("errorMessage");

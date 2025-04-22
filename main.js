b2bc24265ee605d31c0d4bed09dcce63f8f707d1
Mohamad Hossein Askari <askarimohamadhosseinpc@gmail.com>
2025-04-22 16:00:56 +0330

    Add error message and initialize password input styles

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const passwordInput = document.getElementById("password");
 const toggleButton = document.getElementById("togglePassword");
 const errorMessage = document.getElementById("errorMessage");
+
+errorMessage.textContent = "Password Input is Empty";
+errorMessage.classList.remove("hidden");
+errorMessage.style.color = "gray";
+passwordInput.style.borderColor = "blue";
